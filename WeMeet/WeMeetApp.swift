521a896190b670db4229b9119622505a476504ef
monaewhite <monaemwhite@gmail.com>
2024-11-04 18:26:35 -0500

    Initial Commit

---
+++ WeMeet/WeMeetApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeMeetApp.swift
+//  WeMeet
+//
+//  Created by Monae White on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct WeMeetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeMeetTests/WeMeetTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeMeetTests.swift
+//  WeMeetTests
+//
+//  Created by Monae White on 11/4/24.
+//
+
+import Testing
+@testable import WeMeet
+
+struct WeMeetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
